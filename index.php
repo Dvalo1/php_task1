8e2e067cd95f9d248d251043021a200073d7d6b4
Dvalo1 <32309229+Dvalo1@users.noreply.github.com>
2020-09-14 18:49:09 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+	require_once("db.php");
+
+	// if (isset($_GET["name"])) {
+
+	// 	$name = mysql_real_escape_string($conn, $_GET["name"]);
+	// 	$sql = "INSERT INTO `user`(`name`, `password`) 
+	// 		VALUES ('$name','pwd')"
+
+	// 	if ($conn->query($sql) === TRUE) {
+	// 		echo "success";
+	// 	} else {
+	// 		"error:". $conn->error;
+	// 	}
+	// }
+ ?>
